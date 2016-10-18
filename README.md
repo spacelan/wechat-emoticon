effbf3af558326a05922b22e14a378f283d0fe7b
spacelan <spacelan1993@gmail.com>
2016-10-18 11:45:04 +0800

    增加自定义表情抓取示例

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 终端版微信表情轰炸器
 
-1000+自定义表情（未整理，存在奇奇怪怪的表情，慎用，不要搞♂事）
+1000+自定义表情（未整理，存在**不可描述**的福利表情，慎用，不要搞♂事）
 
 ## 运行
 
@@ -27,6 +27,31 @@ node index.js
 
 微信API由[wechat4u](https://github.com/nodeWechat/wechat4u)支持
 
+wechat4u可能是目前唯一能发送表情的JS库
+
 ## 如何抓自定义表情
 
-我想想再写
+微信网页版通过`https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsync`API获取新消息
+
+返回数据中，一个微信消息主要由以下部分组成
+
+```json
+{
+"MsgId": "11973880366746079",
+"FromUserName": "@7f29b95a33028240f631dda0eecb49f",
+"ToUserName": "filehelper",
+"MsgType": 47,
+"Content": "&lt;msg&gt;&lt;emoji fromusername = \"23423asf\" tousername = \"filehelper\" type=\"2\" idbuffer=\"media:0_0\" md5=\"325e6f67bf0bf95b58ca1e0d7cc51821\" len = \"494186\" productid=\"\" androidmd5=\"325e6f67bf0bf95b58ca1e0d7cc51821\" androidlen=\"494186\" s60v3md5 = \"325e6f67bf0bf95b58ca1e0d7cc51821\" s60v3len=\"494186\" s60v5md5 = \"325e6f67bf0bf95b58ca1e0d7cc51821\" s60v5len=\"494186\" cdnurl = \"http://emoji.qpic.cn/wx_emoji/xmrm0xnjtnahibtFOjQ7ywClrojsNQYOPyickj9Yo7D0NEa16DBl5GJA/\" designerid = \"\" thumburl = \"\" encrypturl = \"http://emoji.qpic.cn/wx_emoji/CvEmibBOcGYhrj1gCOCs48L4OdgAtuuyJueljF440oic4r8j6KPRkDfw/\" aeskey= \"caee1393b6427c72e35fef94d277d00b\" width= \"85\" height= \"85\" &gt;&lt;/emoji&gt; &lt;gameext type=\"0\" content=\"0\" &gt;&lt;/gameext&gt;&lt;/msg&gt;",
+...
+}
+```
+
+当`MsgType == 47`为表情消息
+
+然后可以从`Content`中提取`md5`和`cdnurl`（商店表情无法提取）
+
+`md5`用来向别人发送表情
+
+`cdnurl`用来直接下载表情，不需要认证
+
+更详细的请查看代码[index.js](https://github.com/spacelan/wechat-emoticon/blob/master/index.js#L58)
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 'use strict'
+const fs = require('fs')
 const Wechat = require('wechat4u')
 const qrcode = require('qrcode-terminal')
 const emoticons = require('./emoticons.json')
 
 let bot = new Wechat()
 let users = new Set()
+let emoticonSet = new Set()
+
+// 初始化emoticonSet,避免重复记录表情
+try {
+  emoticons.forEach(emoticon => emoticonSet.add(emoticon.md5))
+  fs.closeSync(fs.openSync('./new-emoticons.dat', 'w'))
+  fs.readFileSync('./new-emoticons.dat').split('\n').forEach(emoticon => emoticonSet.add(emoticon.split(
+    ' ')[0]))
+} catch (err) {
+
+}
 
 bot.on('error', err => {
   console.log(err)
@@ -12,9 +24,12 @@ bot.on('error', err => {
 
 bot.on('uuid', uuid => {
   // uuid事件，获取二维码
+  console.log('扫码登录')
   qrcode.generate('https://login.weixin.qq.com/l/' + uuid, {
     small: true
   })
+  console.log('如果二维码不能识别，请打开链接')
+  console.log('https://login.weixin.qq.com/qrcode/' + uuid)
 })
 
 bot.on('login', () => {
@@ -30,6 +45,7 @@ bot.on('message', msg => {
   switch (msg.MsgType) {
     case bot.CONF.MSGTYPE_TEXT:
       // 文本消息
+      // 自定义命令
       if (msg.Content == '科科' && msg.isSendBy(bot.user)) {
         users.add(msg.ToUserName)
         sendEmoticon(msg.ToUserName)
@@ -37,6 +53,19 @@ bot.on('message', msg => {
       } else if (msg.Content == '拜拜' && msg.isSendBy(bot.user)) {
         users.delete(msg.ToUserName)
         console.log('取消轰炸', bot.contacts[msg.ToUserName].getDisplayName())
+      }
+      break
+    case bot.CONF.MSGTYPE_EMOTICON:
+      // 新表情数据写入new-emoticons.dat,去重
+      let pm = msg.Content.match(/md5\s?=\s?"(.*?)".*?cdnurl\s?=\s?"(.*?)"/)
+      if (pm && !emoticonSet.has(pm[1])) {
+        console.log('新表情GET')
+        try {
+          fs.appendFileSync('./new-emoticons.dat', `${pm[1]} ${pm[2]}\n`)
+          emoticonSet.add(pm[1])
+        } catch (err) {
+          console.log(err)
+        }
       }
       break
     default:
@@ -47,11 +76,11 @@ bot.on('message', msg => {
 bot.start()
 
 const sendEmoticon = (ToUserName, index) => {
-  index = index || 0
+  index = index || 0 // 自定义表情顺序
   if (users.has(ToUserName) && index < emoticons.length) {
     bot.sendEmoticon(emoticons[index].md5, ToUserName)
     setTimeout(() => {
-      sendEmoticon(ToUserName, ++index)
-    }, 1000)
+        sendEmoticon(ToUserName, ++index)
+      }, 1000) // 自定义间隔
   }
 }
